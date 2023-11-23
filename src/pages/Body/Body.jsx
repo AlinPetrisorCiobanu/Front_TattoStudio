7f8f99679d134002bbd5b75fd8c562bc4c8c232a
Rumanito12 <ady_9697@yahoo.com>
2023-11-23 16:57:33 +0100

    creación de las rutas de todas las paginas

---
+++ src/pages/Body/Body.jsx
@@ -0,0 +1,32 @@
+import {Navigate , Route ,Routes} from "react-router-dom"
+import { Home } from "../Home/Home"
+import { Login } from "../Login/Login"
+import { Register } from "../Register/Register"
+import { Profile } from "../Profile/Profile"
+import { ModifyUser } from "../ModifyUser/Modify"
+import { DeleteUser } from "../DeleteUser/Delete"
+import { ActivationUser } from "../Activation/Activation"
+import { Galery } from "../Galery/Galeria"
+import { Appoints } from "../CreateAppoints/Appoints"
+import { ModifyAppoints } from "../ModifyAppoints/ModifyAppoints"
+import { DeleteAppoints } from "../DeleteAppoints/DeleteAppoints"
+
+
+export const Body = () =>{
+    return(
+    <Routes>
+        <Route path="*" element={<Navigate to = "/"/> }/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/modify/user" element={<ModifyUser/>}/>
+        <Route path="/delete/user" element={<DeleteUser/>}/>
+        <Route path="/activation" element={<ActivationUser/>}/>
+        <Route path="/galery" element={<Galery/>}/>
+        <Route path="/create/appoints" element={<Appoints/>}/>
+        <Route path="/modify/appoints" element={<ModifyAppoints/>}/>
+        <Route path="/delete/appoints" element={<DeleteAppoints/>}/>
+    </Routes>
+    )
+}
