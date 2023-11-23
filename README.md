fb5f7b4c751be8be1fee42e05d143500d9fab42c
Rumanito12 <ady_9697@yahoo.com>
2023-11-23 16:28:40 +0100

    inicio Proyecto 5.

---
+++ README.md
@@ -0,0 +1,8 @@
+Creación de la parte Frontal del estudio de Tatuajes.
+
+Se van a utilizar los sieguentes lenguajes:
+
+React + TypeScript + Vite
+ReactBootstrap
+Css
+Backend Proyecto 4
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from './common/Header/Header'
+import { Body } from './pages/body/Body'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header/>
+      <Body/>
+    </>
+  )
+}
+
+export default App
